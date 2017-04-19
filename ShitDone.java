d625e16a813cd015f067dbbea02f39253861fe19
bibaroc <sulimovskyy.vladyslav@gmail.com>
2017-04-19 10:10:22 +0200

    Update to the algorithm-
    Uploaded the wring one

--- ShitDone.java
+++ ShitDone.java
@@ -1,127 +1,97 @@
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Scanner;
 
-/**
- * Class meant to compute extremely large calculations over a mathematical
- * expression in PN. Operations are written like: <Operation>
- * <Operator0><Operator1> with a newline or a space as the divider between
- * members.
- * 
- * @author Vladyslav Sulimovskyy
- *
- */
-public class ShitDone {
-
-	List<Number> list = null;
-	static Scanner SC = null;
-static int	THREADCOUNT =0;
-
-	ShitDone(String arg) {
-		list = new ArrayList<Number>();
-		try {
-			SC = new Scanner(new BufferedInputStream(new FileInputStream(new File(arg))));
-			SC.useDelimiter(" ");
-			while (SC.hasNext()) {
-				if (SC.hasNextDouble())
-					list.add(SC.nextDouble());
-				else {
-					switch (SC.next()) {
-					case ("+"):
-						list.add((byte) 1);
-						break;
-					case ("-"):
-						list.add((byte) 2);
-						break;
-					case ("*"):
-						list.add((byte) 3);
-						break;
-					case ("/"):
-						list.add((byte) 4);
-						break;
-					default:
-						throw new Exception();
-					}
-				}
-				// list.add((SC.hasNextDouble() ? SC.nextDouble() : SC.next()));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.print("Finito di importare la robba fra.\r\n");
-	}
-
-	public double getShitDone() throws InterruptedException {
-		boolean operatedThisRun;
-		ArrayList<Thread> threadsRun = new ArrayList<Thread>();
-		do {
-			threadsRun.clear();
-			operatedThisRun = false;
-			for (int i = 0; i < list.size()-2; i++) {
-				System.out.print(list.size() + "\r\n");
-				if (list.get(i) instanceof Byte)
-					if (list.get(i + 1) instanceof Double)
-						if (list.get(i + 2) instanceof Double) {
-							Thread toRun = new Thread(
-									new Evaluator(i, (Double) list.remove(i + 1), (Double) list.remove(i + 1)));
-							toRun.start();
-							threadsRun.add(toRun);
-							THREADCOUNT++;
-							operatedThisRun = true;
-						} else
-							i++;
-			}
-			for (Thread in : threadsRun)
-				in.join();
-		} while (operatedThisRun);
-		System.out.print("Il risultato e: "+list.get(0)+"\r\nUsando: "+THREADCOUNT+" thread.");
-		return 0;
-	}
-
-	public static void main(String args[]) throws InterruptedException {
-
-		// String path = "/home/bibar/Downloads/expression.txt";
-		// String test = "- * / 15 - 7 + 1 1 3 + 2 + 1 1";
-		ShitDone sh = new ShitDone("/home/bibar/Downloads/expression.txt");
-		sh.getShitDone();
-	}
-
-	private class Evaluator implements Runnable {
-		Number operation;
-		Double operand0, operand1;
+public class Shiet {
+	private class Calcola extends Thread {
+		String operator;
 		int index;
+		double result;
 
-		Evaluator(Number operator, double operand0, double operand1) {
-			this.operation = list.get((int) operator);
-			this.operand0 = operand0;
-			this.operand1 = operand1;
-			this.index = (int) operator;
+		public double get() {
+			return result;
+		}
+
+		Calcola(String index) {
+			this.operator = index;
 		}
 
 		@Override
 		public void run() {
-			// System.out.print("Sto facendo: " + operand0 + operation +
-			// operand1 + "\r\n");
-			switch ((byte) operation) {
-			case (1):
-				list.set(index, operand0 + operand1);
+			THREADCOUNT++;
+			double operator0, operator1;
+			if (sc.hasNextDouble())
+				operator0 = sc.nextDouble();
+			else {
+				Calcola c = new Calcola(sc.next());
+				c.start();
+				try {
+					c.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				operator0 = c.get();
+			}
+			if (sc.hasNextDouble())
+				operator1 = sc.nextDouble();
+			else {
+				Calcola c = new Calcola(sc.next());
+				c.start();
+				try {
+					c.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				operator1 = c.get();
+			}
+			switch (operator) {
+			case ("+"):
+				result = operator0 + operator1;
 				break;
-			case (2):
-				list.set(index, operand0 - operand1);
+			case ("-"):
+				result = operator0 - operator1;
 				break;
-			case (3):
-				list.set(index, operand0 * operand1);
+			case ("*"):
+				result = operator0 * operator1;
 				break;
-			case (4):
-				list.set(index, operand0 / operand1);
+			case ("/"):
+				result = operator0 / operator1;
 				break;
 			}
-		}
+			// TODO Auto-generated method stub
 
+		}
 	}
+
+	private static  int THREADCOUNT = 0;
+
+	
+	static Scanner sc = null;
+
+	Shiet(String arg) {
+		try {
+			long time = System.nanoTime();
+			
+			sc = new Scanner(new BufferedInputStream(new FileInputStream(new File(arg))));
+			sc.useDelimiter(" ");
+			Calcola c = new Calcola(sc.next());
+			c.start();
+			c.join();
+			System.out.print("Il risultato e': " + c.get() + "\r\n");
+			System.out.print("Computato in: "+(System.nanoTime()-time)/1000000000+" secondi\r\n");
+			System.out.print("Usando: "+THREADCOUNT+" thread\r\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Shiet sh = new Shiet("/home/bibar/Downloads/expression.txt");
+	}
+
 }
