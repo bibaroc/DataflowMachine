70161bc6dabdf99b25be06a9eb6e3a2bbd45b7a2
bibaroc <sulimovskyy.vladyslav@gmail.com>
2017-04-19 09:24:26 +0200

    Update ShitDone.java

--- ShitDone.java
+++ ShitDone.java
@@ -1,10 +1,11 @@
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Class meant to compute extremely large calculations over a mathematical
@@ -16,96 +17,111 @@ import java.util.concurrent.Executors;
  *
  */
 public class ShitDone {
-	// Service pool to control the number of thread spawned and the values
-	// returned from the calculation.
-	private static ExecutorService POOL = null;
-	// Using java.util.Scanner because of it's ability to distinguish doubles
-	// from operations.
-	private static Scanner SC = null;
+
+	List<Number> list = null;
+	static Scanner SC = null;
+static int	THREADCOUNT =0;
 
 	ShitDone(String arg) {
+		list = new ArrayList<Number>();
 		try {
-			SC = new Scanner(new File(arg));
+			SC = new Scanner(new BufferedInputStream(new FileInputStream(new File(arg))));
 			SC.useDelimiter(" ");
-		} catch (FileNotFoundException e) {
-			System.err.println("Looks like " + arg + " is not a valid file.");
-			// Using error code 404 for FileNotFound.
-			System.exit(404);
+			while (SC.hasNext()) {
+				if (SC.hasNextDouble())
+					list.add(SC.nextDouble());
+				else {
+					switch (SC.next()) {
+					case ("+"):
+						list.add((byte) 1);
+						break;
+					case ("-"):
+						list.add((byte) 2);
+						break;
+					case ("*"):
+						list.add((byte) 3);
+						break;
+					case ("/"):
+						list.add((byte) 4);
+						break;
+					default:
+						throw new Exception();
+					}
+				}
+				// list.add((SC.hasNextDouble() ? SC.nextDouble() : SC.next()));
+			}
 		} catch (Exception e) {
-			System.err.println("General exception during the scanner creation.");
-			// Using 500 for general internal error.
-			System.exit(500);
+			e.printStackTrace();
 		}
-		POOL = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+		System.out.print("Finito di importare la robba fra.\r\n");
 	}
 
-	public double getShitDone() {
-		try {
-			double result = POOL.submit(new Evaluator(SC.next())).get();
-			System.out.println("The result of the computation was: " + result);
-			POOL.shutdown();
-			if (SC != null)
-				SC.close();
-			return result;
-		} catch (InterruptedException e) {
-			System.err.println("The main thread was interrupted in a bad way.");
-			// 501 for bad interruption.
-			System.exit(501);
-		} catch (ExecutionException e) {
-			System.err.println("An unexpected error occured during execution.");
-			System.exit(500);
-		}
-		return -1.0;
+	public double getShitDone() throws InterruptedException {
+		boolean operatedThisRun;
+		ArrayList<Thread> threadsRun = new ArrayList<Thread>();
+		do {
+			threadsRun.clear();
+			operatedThisRun = false;
+			for (int i = 0; i < list.size()-2; i++) {
+				System.out.print(list.size() + "\r\n");
+				if (list.get(i) instanceof Byte)
+					if (list.get(i + 1) instanceof Double)
+						if (list.get(i + 2) instanceof Double) {
+							Thread toRun = new Thread(
+									new Evaluator(i, (Double) list.remove(i + 1), (Double) list.remove(i + 1)));
+							toRun.start();
+							threadsRun.add(toRun);
+							THREADCOUNT++;
+							operatedThisRun = true;
+						} else
+							i++;
+			}
+			for (Thread in : threadsRun)
+				in.join();
+		} while (operatedThisRun);
+		System.out.print("Il risultato e: "+list.get(0)+"\r\nUsando: "+THREADCOUNT+" thread.");
+		return 0;
 	}
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws InterruptedException {
 
-		String path = "/home/bibar/Desktop/shieeet.txt";
-		ShitDone shieeet = new ShitDone(path);
-		shieeet.getShitDone();
-
+		// String path = "/home/bibar/Downloads/expression.txt";
+		// String test = "- * / 15 - 7 + 1 1 3 + 2 + 1 1";
+		ShitDone sh = new ShitDone("/home/bibar/Downloads/expression.txt");
+		sh.getShitDone();
 	}
 
-	private class Evaluator implements Callable<Double> {
-		// Value used to evaluate the expression and return the result.
-		double operand0, operand1;
-		// Operation to perform on the operands.
-		String operation;
+	private class Evaluator implements Runnable {
+		Number operation;
+		Double operand0, operand1;
+		int index;
 
-		Evaluator(String operation) {
-			// Only sum difference multiplication and division are supported.
-			if (!operation.equals("/") && !operation.equals("*") && !operation.equals("+") && !operation.equals("-"))
-				throw new IllegalArgumentException("Apparently the file is broken or does not conform to the PN");
-			// At this point i know the operation is supported.
-			this.operation = operation;
+		Evaluator(Number operator, double operand0, double operand1) {
+			this.operation = list.get((int) operator);
+			this.operand0 = operand0;
+			this.operand1 = operand1;
+			this.index = (int) operator;
 		}
 
 		@Override
-		public Double call() throws Exception {
-			// If the value after the operation is not a double it must be an
-			// operation and i must resolve it first.
-			if (SC.hasNextDouble())
-				operand0 = SC.nextDouble();
-			else
-				operand0 = POOL.submit(new Evaluator(SC.next())).get();
-			if (SC.hasNextDouble())
-				operand1 = SC.nextDouble();
-			else
-				operand1 = POOL.submit(new Evaluator(SC.next())).get();
-			// Switch decision on the operation over the operands.
-			switch (operation) {
-			case ("+"):
-				return operand0 + operand1;
-			case ("-"):
-				return operand0 - operand1;
-			case ("*"):
-				return operand0 * operand1;
-			case ("/"):
-				return operand0 / operand1;
-			// Not really necessary.
-			default:
-				throw new IllegalArgumentException("It seems the file contains an unsupported operation.");
+		public void run() {
+			// System.out.print("Sto facendo: " + operand0 + operation +
+			// operand1 + "\r\n");
+			switch ((byte) operation) {
+			case (1):
+				list.set(index, operand0 + operand1);
+				break;
+			case (2):
+				list.set(index, operand0 - operand1);
+				break;
+			case (3):
+				list.set(index, operand0 * operand1);
+				break;
+			case (4):
+				list.set(index, operand0 / operand1);
+				break;
 			}
 		}
+
 	}
 }
