94674311f4d03d3590b0f6761f8c4dc62e43df86
SulimovskyyVladyslav <sulimovskyy.vladyslav@gmail.com>
2017-05-23 13:00:47 +0200

    Fixing stackOverflow errors, hash mappings and commenting out.

--- src/ConcurrentCompute.java
+++ src/ConcurrentCompute.java
@@ -4,30 +4,27 @@ import java.util.Scanner;
 
 public class ConcurrentCompute {
 
+	// Scanner to read from file.
 	private static Scanner SC = null;
 	// Left son's index: 2i+1
 	// Right son's index: 2i+2
 	// Father's index: floor((i-1)/2)
 	private static HashMap<Integer, Object> tree;
-	static int i = 0;
 
+	// The builder imports the file in memory all by itself.
 	ConcurrentCompute(String pathToExpression) {
 		try {
-			SC = new Scanner(new BufferedInputStream(new FileInputStream(pathToExpression)));
-			SC.useDelimiter(" ");
-			
-			if (SC.hasNextDouble() || !SC.hasNext())
-				throw new IllegalStateException("Sorry seems like the file is misconstructed");
+			// Get the scanner and the hashMap
+			SC = new Scanner(new File(pathToExpression));
 			tree = new HashMap<>();
-			//int i = 0;
-			//String ob;
-			//while(SC.hasNext()){
-			//	i++;
-			//	ob = SC.next();
-			//	tree.put(ob.hashCode(), ob);}
-			//System.out.print(i+"\r\n");
-		//	System.exit(0);
-			populateTree(0);
+			// If it is either an epty file or it starts with a double throw
+			// IllegalStateException.
+			if (SC.hasNextDouble() || !SC.hasNext()) {
+				SC.close();
+				throw new IllegalStateException("Sorry seems like the file is misconstructed");
+			} else
+				// If it seems ok, start polulating the "Tree"
+				populateTree(0);
 			SC.close();
 			System.out.print("Just finished reading the file.\r\n");
 		} catch (FileNotFoundException e) {
@@ -37,40 +34,29 @@ public class ConcurrentCompute {
 	}
 
 	private void populateTree(int currentIndex) {
-		System.out.print(i + "\r\n");
 		// I know it is a string
-		i++;
 		tree.put(currentIndex, SC.next());
-		// if the char following the operator exists
-		if (SC.hasNext())
-			// And it is a double
-			if (SC.hasNextDouble()){
-				i++;
-				tree.put(2 * currentIndex + 1, SC.nextDouble());}
-			// Else it is a String
-			else
-				populateTree(2 * currentIndex + 1);
-		// If after an operator there are no operands
+		// If the first operand is a double i put it in place
+		if (SC.hasNextDouble())
+			tree.put(2 * currentIndex + 1, SC.nextDouble());
+		// Or if it's a string, i can build a sub tree to mark that expression.
+		else if (SC.hasNext())
+			populateTree(2 * currentIndex + 1);
+		// If there is no operand, the file is misconstructed.
 		else
 			throw new IllegalStateException("Sorry seems like the file is misconstructed");
-		// Looking for the second element
-		if (SC.hasNext())
-			// If it's a double
-			if (SC.hasNextDouble()){
-				i++;
-				tree.put(2 * currentIndex + 2, SC.nextDouble());}
-			// Or maybe a String
-			else
-				populateTree(2 * currentIndex + 2);
-		// If there is no second element
+		// Same for the second operand
+		if (SC.hasNextDouble())
+			tree.put(2 * currentIndex + 2, SC.nextDouble());
+		else if (SC.hasNext())
+			populateTree(2 * currentIndex + 2);
 		else
 			throw new IllegalStateException("Sorry seems like the file is misconstructed");
-		}
+	}
 
 	public static void main(String[] args) {
-		ConcurrentCompute cc = new ConcurrentCompute("C:/expression.txt");
-		for(Integer in: tree.keySet())
-			System.out.print(tree.get(in)+" ");
+		ConcurrentCompute cc = new ConcurrentCompute(
+				"C:/Users/Bibaroc/Desktop/Workspace/DataFlowMachine/expression.txt");
 	}
 
 }
